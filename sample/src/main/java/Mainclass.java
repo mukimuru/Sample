a4e62db7e455f03d0c4722f5da986b565508cb91
mukimuru <mukimuru@DIN78000444.corp.capgemini.com>
2019-07-04 14:51:38 +0530

    Commited

---
+++ sample/src/main/java/Mainclass.java
@@ -0,0 +1,14 @@
+
+
+public class Mainclass {
+	public static void main(String[] args) {
+		System.out.println("welcome! sales Application!");
+		
+	
+		
+	}
+	public static void display() {
+		System.out.println(" Good Morning!");
+	}
+
+}
---
+++ sample/src/main/java/SalesClass.java
@@ -0,0 +1,11 @@
+
+
+public class SalesClass {
+private int[] arr= {1,34,56,7};
+public void show() {
+	for(int num:arr) {
+		System.out.println(num);;
+		
+	}
+}
+}
